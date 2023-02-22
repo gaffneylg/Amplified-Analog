620465bcf06c00409bba780edbe16de7d1a5692a
Liam Gaffney <gaffneylg@gmail.com>
2023-02-22 22:39:37 +0000

    roman numerals and stats location update

--- app/index.js
+++ app/index.js
@@ -6,14 +6,79 @@ console.log("App code started");
 
 import clock from "clock";
 import * as document from "document";
-import { preferences } from "user-settings";
+
+import * as health from "user-activity";
+import {HeartRateSensor} from "heart-rate";
+import {display} from "display";
+import {vibration} from "haptics";
+import {peerSocket} from "messaging";
+import {preferences, units} from "user-settings";
+import {me} from "appbit";
+import {user} from "user-profile";
+import * as fs from "fs";
+import {battery} from "power";
+import {decode} from "cbor";
+import {inbox} from "file-transfer";
+
+
+const THEMES = {
+  red:    ["F93535", "CC4848", "AB4545"],
+  orange: ["FF970F", "DD7F23", "B3671D"],
+  yellow: ["FFFF00", "E4DB4A", "C6BC1E"],
+  green:  ["14C610", "119E0E", "0D730B"],
+  blue:   ["6fa8e9", "5682b4", "32547a"],
+  purple: ["E86FE9", "B455B5", "79327A"],
+  navy: ["5555ff", "4444ff", "4444ff"],
+  grey: ["888888", "666666", "444444"],
+  white: ["FFFFFF", "FFFFFF", "FFFFFF"]
+};
+const HOUR12 = (preferences.clockDisplay === "12h");
+const PROFILE = me.permissions.granted("access_user_profile");
+// const NOCLIMB = (health.today.local.elevationGain === undefined);
+
+let weekNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+let lastUpdatedRings = 0;
+let lastUpdatedHeart = 0;
+let showRings = true;
+let unboldStats = false;
+
+
+// let stats = NOCLIMB ? ["none", "steps", "heart", "cals", "mins", "time"] : ["none", "steps", "heart", "floors", "cals", "mins", "time"];
+let stats = ["none", "steps", "heart", "cals", "mins", "time"]
+let firstStat = 0;  //0=blank
+let curStat = 0;
+let heartSensor;
+
+let myDate = $("mydate");
+let myWeek = $("myweek");
+let myHours = $("hours");
+let myMins = $("minutes");
+let mySecs = $("seconds");
+let myStats = $("mystats");
+let statIcon = $("staticon");
+let myBatt = $("batt");
+let twelve = $("twelve");
+let three = $("three");
+let six = $("six");
+let nine = $("nine");
+
+let myRingTL = $("today_tl");
+let myRingTR = $("today_tr");
+let myRingBL = $("today_bl");
+let myRingBR = $("today_br");
+
+function $(s) {
+  return document.getElementById(s);
+}
+
 
 // Tick every second
 clock.granularity = "seconds";
 
 let hourHand = document.getElementById("hours");
-let minHand = document.getElementById("mins");
-let secHand = document.getElementById("secs");
+let minHand = document.getElementById("minutes");
+let secHand = document.getElementById("seconds");
 
 // Returns an angle (0-360) for the current hour in the day, including minutes
 function hoursToAngle(hours, minutes) {
@@ -34,15 +99,286 @@ function secondsToAngle(seconds) {
 
 // Rotate the hands every tick
 function updateClock() {
-  let today = new Date();
-  let hours = today.getHours() % 12;
-  let mins = today.getMinutes();
-  let secs = today.getSeconds();
+  let now = new Date();
+  myDate.text = now.getDate();
+  myWeek.text = weekNames[now.getDay()];
+  twelve.text = "XII";
+  three.text = "III";
+  six.text = "VI";
+  nine.text = "IX";
+  statIcon = "";
+
+  let hours = now.getHours() % 12;
+  let mins = now.getMinutes();
+  let secs = now.getSeconds();
 
   hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
   minHand.groupTransform.rotate.angle = minutesToAngle(mins);
   secHand.groupTransform.rotate.angle = secondsToAngle(secs);
+
+  myBatt.x2 = Math.round(battery.chargeLevel*7/25) - 14;
+
+  if(showRings || (stats.length > 0 && stats[curStat] !== "none")) {
+    let nowTime = now.getTime();
+    
+    if(showRings) {
+      if(nowTime - lastUpdatedRings > 30000) {
+        lastUpdatedRings = nowTime;
+        let today = health.today.adjusted;
+        let goal = health.goals;
+        // updateRing(myRingTL, "cal", goal, today);
+        // updateRing(myRingTR, "step", goal, today);
+        // updateRing(myRingBR, "dist", goal, today);
+        // if(NOCLIMB) {
+        //   updateRing(myRingBL, "active", goal, today);
+        // } else {
+        //   updateRing(myRingBL, "climb", goal, today);
+        // }
+      }
+    }
+
+    if(stats.length > 0 && stats[curStat] !== "none" && !display.aodActive) {
+      if(stats[curStat] !== "heart") {
+        updateStat();
+      } else {
+        if(nowTime - lastUpdatedHeart > 1600) {
+          lastUpdatedHeart = nowTime;
+          updateHeart();
+        }
+      }
+    } else {
+      myStats.text = "";
+      statIcon.href = "";
+    }
+  }
+}
+
+function setAOD(on) {
+  if(on) {
+    clock.granularity = "minutes";
+    secHand.style.display = "none";
+    myStats.style.display = "none";
+    // statIcon.style.display = "none";
+  } else {
+    clock.granularity = "seconds";  
+    secHand.style.display = "inline";
+    myStats.style.display = "inline";
+    // statIcon.style.display = "inline";
+  }
+}
+
+if(display.aodAvailable && me.permissions.granted("access_aod")) {
+  display.aodAllowed = true;
+  display.onchange = () => {
+    setAOD(display.aodActive);
+    if(!display.aodActive) onTick();
+  };
+  setAOD(display.aodActive);
+} else {
+  clock.granularity = "seconds";
 }
 
 // Update the clock every tick event
 clock.addEventListener("tick", updateClock);
+
+
+$("top_half").onclick = () => {
+  if(!display.aodEnabled) {
+    if(display.autoOff === true) {
+      display.autoOff = false;
+      if(!display.autoOff) $("bklight").style.display = "inline";
+    } else {
+      display.autoOff = true;
+      if(display.autoOff) $("bklight").style.display = "none";
+    }
+  }
+};
+
+$("btm_half").onclick = () => {
+  if(stats.length > 0) {
+    curStat = (curStat + 1) % stats.length;
+    if(stats[curStat] === "heart") {
+      updateHeart();
+    } else {
+      updateStat();
+    }
+  }
+};
+
+function updateRing(node, holder, goal, today) {
+  let angle = 0;
+  // if(holder === "cal") {
+  //   angle = (today.calories || 0)*360/(goal.calories || 400);
+  // } else 
+  if(holder === "step") {
+    angle = (today.steps || 0)*360/(goal.steps || 10000);
+  } 
+  // else if(holder === "dist") {
+  //   angle = (today.distance || 0)*360/(goal.distance || 7200);
+  // } else if(holder === "climb") {
+  //   angle = (today.elevationGain || 0)*360/(goal.elevationGain || 20);
+  // } else if(holder === "active") {
+  //   angle = (today.activeZoneMinutes.total || 0)*360/(goal.activeZoneMinutes.total || 30);
+  // }
+  node.sweepAngle = Math.min(360, Math.round(angle));
+}
+
+function updateStat() {
+  let today = health.today.adjusted;
+  switch(stats[curStat]) {
+    case "steps":
+      myStats.text = today.steps; 
+      
+      let stepsIcon = "./resources/ico_steps.png";
+      statIcon.href = stepsIcon;
+      // statIcon.href 
+      // statIcon.style.display = stepsIcon;
+
+      break;
+    case "heart":
+      updateHeart();
+      break;
+    // case "dist": 
+    //   myStats.text = (units.distance === "metric") ? round(today.distance/1000) + " km" : round(today.distance/1609.34) + " mi";
+    //   break;
+    // case "floors":
+    //   myStats.text = today.elevationGain + " f";
+    //   break;
+    // case "cals":
+    //   myStats.text = today.calories + " cal";
+    //   break;
+    // case "mins":
+    //   let t = today.activeZoneMinutes.total;
+    //   myStats.text = Math.floor(t/60) + "' " + pad(t % 60) + '"';
+    //   break;
+    // case "time":
+    //   let t = new Date();
+    //   let hr = t.getHours();
+    //   myStats.text = ((hr > 12 && HOUR12) ? hr % 12 : hr) + ":" + pad(t.getMinutes());
+    //   break;
+    // case "weight":
+    //   myStats.text = !PROFILE ? "No Access" : (units.weight === "metric" ? round(user.weight) + " kg" : round(user.weight/2.2046) + " lb");
+    //   break;
+    // case "rest":
+    //   myStats.text = !PROFILE ? "No Access" : "- " + user.restingHeartRate + " -";
+    //   break;
+    case "batt":
+      myStats.text = battery.chargeLevel + "%";
+      // <image x="44%+10" y="70%" width="23" height="23" href="ico_batt.png" fill="#de5959"/>
+
+      break;
+    default: myStats.text = "";
+  }
+}
+
+function pad(n) {
+  return n < 10 ? "0" + n : n;
+}
+
+function round(n) {
+  n = n.toFixed(2);
+  if(n.substr(-2) === "00") return n.substr(0, n.length - 3);
+  if(n.substr(-1) === "0") return n.substr(0, n.length - 1);
+  return n;
+}
+
+var delayHeart;
+
+function updateHeart() {
+  let h = heartSensor;
+  if(!h) {
+    heartSensor = h = new HeartRateSensor();
+    h.onreading = () => {
+      setTimeout(() => h.stop(), 100);
+      clearTimeout(delayHeart);
+      myStats.text = h.heartRate;
+      // myStats.text = "heart";
+    };
+    h.onerror = () => {
+      setTimeout(() => h.stop(), 100);
+      clearTimeout(delayHeart);
+      myStats.text = "--";
+    };
+  }
+  if(!h.activated) {
+    clearTimeout(delayHeart);
+    delayHeart = setTimeout(() => {
+      myStats.text = "--";
+    }, 500);
+    h.start();
+  }
+}
+
+function applySettings(o) {
+  if(o.theme) {
+    let colors = THEMES[o.theme] || [];
+    for(let i = 0; i < colors.length; i++) {
+      let nodes = document.getElementsByClassName("color" + (i + 1));
+      let node, j = 0;
+      while(node = nodes[j++]) node.style.fill = "#" + colors[i];
+    }
+  }
+  if(o.days) {
+    weekNames = o.days;
+  }
+  if("hideRings" in o) {
+    showRings = !o.hideRings;
+    let nodes = document.getElementsByClassName("rings");
+    let node, j = 0;
+    while(node = nodes[j++]) node.style.display = showRings ? "inline" : "none";
+  }
+  if("unboldStats" in o) {
+    unboldStats = o.unboldStats;
+    myStats.style.fontFamily = unboldStats ? "System-Regular" : "System-Bold";
+  }
+  if("stats" in o) stats = o.stats;
+  if("firstStat" in o) curStat = firstStat = Math.min(o.firstStat, stats.length - 1);
+  myStats.text = "";
+  lastUpdatedRings = 0;
+  lastUpdatedHeart = 0;
+}
+
+function parseFile(name) {
+  let obj;
+  try {
+    obj = fs.readFileSync(name, "cbor");
+  } catch(e) {
+    return true;
+  }
+
+  if(name === "settings2.txt") {
+    if(obj) applySettings(obj);
+  }
+}
+
+function pendingFiles() {
+  let found = false;
+  let temp;
+  while(temp = inbox.nextFile()) {
+    parseFile(temp);
+    found = true;
+  }
+  if(found) {
+    display.poke();
+    vibration.start("bump");
+  }
+}
+
+pendingFiles();
+inbox.onnewfile = pendingFiles;
+
+if(parseFile("settings2.txt")) {
+  let done = (peerSocket.readyState === peerSocket.OPEN);
+  if(done) {
+    peerSocket.send({getAll: 1});
+  } else {
+    peerSocket.onopen = () => {
+      if(!done) peerSocket.send({getAll: 1});
+      done = true;
+    };
+  }
+}
+
+// if(NOCLIMB) {
+//   $("floors").href = "ico_azonem.png";
+// }
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,13 +1,82 @@
-registerSettingsPage(({ settings }) => (
-  <Page>
-    <Section
-      title={
-        <Text bold align="center">
-          App Settings
-        </Text>
-      }
-    >
-      <Text>Hello world!</Text>
-    </Section>
-  </Page>
-));
+function isNotEmpty(list) {
+  list = list && JSON.parse(list);
+  list = list && list.values;
+  return list && (list.length === 1 ? list[0].value !== "none" : list.length > 1);
+}
+
+//stats = {selected: [1, 0, 2], values[{name:"None",value:"none"},{name:"Steps",value:"steps"},{name:"Heart Rate (bpm)",value:"heart"}]}
+
+//firstStat = {"values":[{"name":"Steps","value":"steps"}],"selected":[1]}
+
+function getClampedFirstStat(props) {
+  let first = props.settingsStorage.getItem("firstStat2");
+  let stats = props.settingsStorage.getItem("stats");
+  if(first && stats) {
+    return Math.min(JSON.parse(first).selected[0], JSON.parse(stats).selected.length - 1);
+  }
+}
+
+registerSettingsPage(props => {
+  let statsList = [
+    {name: "None", value: "none"},
+    {name: "Steps", value: "steps"},
+    {name: "Heart Rate (bpm)", value: "heart"},
+    {name: "Resting Heart Rate (- bpm -)", value: "rest"},
+    {name: "Distance (km/mi)", value: "dist"},
+    // {name: "Floors Climbed (f)", value: "floors"},
+    // {name: "Calories Burned (cal)", value: "cals"},
+    // {name: "Active Zone Minutes (hh'mm'')", value: "mins"},
+    // {name: "Weight (kg/lb)", value: "weight"},
+    {name: "Digital Time", value: "time"},
+    {name: "Battery", value: "batt"}
+  ];
+
+  if(/Versa Lite/.test(props.settings.modelName)) {
+    statsList = statsList.filter(n => n.value !== "floors");
+  }
+
+  return (
+    <Page>
+      <Section title="Color">
+        <ColorSelect
+          settingsKey="theme"
+          centered={true}
+          colors={[
+            {color: "tomato", value: "red"},
+            {color: "sandybrown", value: "orange"},
+            {color: "gold", value: "yellow"},
+            {color: "lawngreen", value: "green"},
+            {color: "deepskyblue", value: "blue"},
+            {color: "plum", value: "purple"},
+            {color: "mediumblue", value: "navy"},
+            {color: "grey", value: "grey"},
+            {color: "white", value: "white"}
+          ]}
+        />      
+      </Section>
+
+      <Section title="Health Rings">
+        <Toggle settingsKey="hideRings" label="Hide rings" />
+      </Section>  
+
+      <Section title="Health Stats" description="Tap on bottom half of watch face to cycle through the stats.">
+        <Select label="Show Stats"
+          multiple
+          selectViewTitle="Show Stats"
+          settingsKey="stats"
+          options={statsList}
+        />        
+
+        {(isNotEmpty(props.settings.stats) ?
+          <Select label="Default"
+            selectViewTitle="Default Stat"
+            options={JSON.parse(props.settings.stats).values}
+            onSelection={sel => props.settingsStorage.setItem("firstStat2", JSON.stringify(sel))}
+            selected={getClampedFirstStat(props)}
+          /> : null)}
+       
+        <Toggle settingsKey="unboldStats" label="Narrow Font" />
+      </Section>
+    </Page>
+  );
+});
