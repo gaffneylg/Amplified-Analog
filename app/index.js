bcf7ec0bea83cfcc44c06d38f252c5e3ba744b45
Liam Gaffney <gaffneylg@gmail.com>
2023-04-25 17:21:22 +0100

    code cleanup for publishing

--- app/index.js
+++ app/index.js
@@ -5,32 +5,11 @@
 console.log("App code started");
 
 import * as document from "document";
-// import * as fs from "fs";
 import * as health from "user-activity";
 import {battery} from "power";
 import {clock} from "clock";
 import {display} from "display";
 import {HeartRateSensor} from "heart-rate";
-// import {peerSocket} from "messaging";
-// import {inbox} from "file-transfer";
-// import {vibration} from "haptics";
-// import {decode} from "cbor";
-// import {me} from "appbit";
-// import {preferences, units} from "user-settings";
-// import {user} from "user-profile";
-
-
-const THEMES = {
-  red:    ["F93535", "CC4848", "AB4545"],
-  orange: ["FF970F", "DD7F23", "B3671D"],
-  yellow: ["FFFF00", "E4DB4A", "C6BC1E"],
-  green:  ["14C610", "119E0E", "0D730B"],
-  blue:   ["6fa8e9", "5682b4", "32547a"],
-  purple: ["E86FE9", "B455B5", "79327A"],
-  navy:   ["5555ff", "4444ff", "4444ff"],
-  grey:   ["888888", "666666", "444444"],
-  white:  ["FFFFFF", "FFFFFF", "FFFFFF"]
-};
 
 let lastUpdatedHeart = 0;
 let stats = ["steps", "heart", "batt"]
@@ -131,37 +110,26 @@ $("btm_half").onclick = () => {
 
 function updateStat() {
   let today = health.today.adjusted;
-  var statIconSvg = document.getElementById('statIconSvg');
-  var statIcon = statIconSvg.getElementById('statIcon');
   switch (stats[curStat]) {
     case "steps":
       myStats.text = today.steps;
-      
+      var statIconSvg = document.getElementById('statIconSvg');
+      var statIcon = statIconSvg.getElementById('statIcon');
       statIcon.href = "./resources/icon_steps.png";
       break;
     case "batt":
       myStats.text = battery.chargeLevel + "%";
+      var statIconSvg = document.getElementById('statIconSvg');
+      var statIcon = statIconSvg.getElementById('statIcon');
       let battIcon = "./resources/icon_battery.png";
-      // var statIconSvg = document.getElementById('statIconSvg');
-      // var statIcon = statIconSvg.getElementById('statIcon');
       statIcon.href = battIcon;
       break;
     default:
       myStats.text = "";
-      // var statIconSvg = document.getElementById('statIconSvg');
-      // var statIcon = statIconSvg.getElementById('statIcon');
       statIcon.href = "";
       break;
   }
 }
-
-// function steps() {
-//   let today = health.today.adjusted;
-//   myStats.text = today.steps;
-//   var statIconSvg = document.getElementById('statIconSvg');
-//   var statIcon = statIconSvg.getElementById('statIcon');
-//   statIcon.href = "./resources/icon_steps.png";
-// }
 
 var delayHeart;
 
@@ -193,167 +161,3 @@ function updateHeart() {
     h.start();
   }
 }
-
-// function applySettings(o) {
-//   if (o.theme) {
-//     let colors = THEMES[o.theme] || [];
-//     for (let i = 0; i < colors.length; i++) {
-//       let nodes = document.getElementsByClassName("color" + (i + 1));
-//       let node, j = 0;
-//       while (node = nodes[j++]) node.style.fill = "#" + colors[i];
-//     }
-//   }
-//   if (o.days) {
-//     weekNames = o.days;
-//   }
-//   if ("stats" in o) 
-//     stats = o.stats;
-//     myStats.text = "";
-//     lastUpdatedHeart = 0;
-// }
-
-// function parseFile(name) {
-//   let obj;
-//   try {
-//     obj = fs.readFileSync(name, "cbor");
-//   } catch (e) {
-//     return true;
-//   }
-
-//   if (name === "settings2.txt") {
-//     if (obj) applySettings(obj);
-//   }
-// }
-
-// if (parseFile("settings2.txt")) {
-//   let done = (peerSocket.readyState === peerSocket.OPEN);
-//   if (done) {
-//     peerSocket.send({ getAll: 1 });
-//   } else {
-//     peerSocket.onopen = () => {
-//       if (!done) peerSocket.send({ getAll: 1 });
-//       done = true;
-//     };
-//   }
-// }
-
-// if(display.aodAvailable && me.permissions.granted("access_aod")) {
-//   display.aodAllowed = true;
-//   display.onchange = () => {
-//     setAOD(display.aodActive);
-//     if(!display.aodActive) onTick();
-//   };
-//   setAOD(display.aodActive);
-// } else {
-//   clock.granularity = "seconds";
-// }
-
-
-// does the device support AOD, and can I use it?
-// if (display.aodAvailable && me.permissions.granted("access_aod")) {
-//   // tell the system we support AOD
-//   display.aodAllowed = true;
-
-//   // respond to display change events
-//   display.addEventListener("change", () => {
-//     // Is AOD inactive and the display is on?
-//     if (!display.aodActive && display.on) {
-//       console.log("AOD false");
-
-//       clock.granularity = "seconds";
-//       // clock.granularity = "seconds";  
-//       secHand.style.display = "inline";
-//       myStats.style.display = "inline";
-//       // Show elements & start sensors
-//       // someElement.style.display = "inline";
-//       // hrm.start();
-//     } else {
-//       clock.granularity = "minutes";
-//       console.log("AOD true");
-
-//       // Hide elements & stop sensors
-//       secHand.style.display = "none";
-//       myStats.style.display = "none";
-//       // someElement.style.display = "none";
-//       // hrm.stop();
-//     }
-//   });
-// }
-
-// function updateRing(node, holder, goal, today) {
-//   let angle = 0;
-//   // if(holder === "cal") {
-//   //   angle = (today.calories || 0)*360/(goal.calories || 400);
-//   // } else 
-//   if(holder === "step") {
-//     angle = (today.steps || 0)*360/(goal.steps || 10000);
-//   } 
-//   // else if(holder === "dist") {
-//   //   angle = (today.distance || 0)*360/(goal.distance || 7200);
-//   // } else if(holder === "climb") {
-//   //   angle = (today.elevationGain || 0)*360/(goal.elevationGain || 20);
-//   // } else if(holder === "active") {
-//   //   angle = (today.activeZoneMinutes.total || 0)*360/(goal.activeZoneMinutes.total || 30);
-//   // }
-//   node.sweepAngle = Math.min(360, Math.round(angle));
-// }
-
-
-// case "heart":
-//   let heartIcon = "./resources/icon_heart.png";
-//   var statIconSvg = document.getElementById('statIconSvg');
-//   var statIcon = statIconSvg.getElementById('statIcon');
-//   statIcon.href = heartIcon;
-//   updateHeart();
-//   break;
-
-// function pad(n) {
-//   return n < 10 ? "0" + n : n;
-// }
-
-// function round(n) {
-//   n = n.toFixed(2);
-//   if(n.substr(-2) === "00") return n.substr(0, n.length - 3);
-//   if(n.substr(-1) === "0") return n.substr(0, n.length - 1);
-//   return n;
-// }
-
-// $("top_half").onclick = () => {
-//   if (!display.aodEnabled) {
-//     if (display.autoOff === true) {
-//       display.autoOff = false;
-//       if (!display.autoOff) $("bklight").style.display = "inline";
-//     } else {
-//       display.autoOff = true;
-//       if (display.autoOff) $("bklight").style.display = "none";
-//     }
-//   }
-// };
-
-// function setAOD(on) {
-//   if (on) {
-//     clock.granularity = "minutes";
-//     secHand.style.display = "none";
-//     myStats.style.display = "none";
-//   } else {
-//     clock.granularity = "seconds";
-//     secHand.style.display = "inline";
-//     myStats.style.display = "inline";
-//   }
-// }
-
-// function pendingFiles() {
-//   let found = false;
-//   let temp;
-//   while (temp = inbox.nextFile()) {
-//     parseFile(temp);
-//     found = true;
-//   }
-//   if (found) {
-//     display.poke();
-//     vibration.start("bump");
-//   }
-// }
-
-// pendingFiles();
-// inbox.onnewfile = pendingFiles;
